eff19befa067c9dd454fda341ff6a8c7fc41037b
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-19 12:03:01 +0000

    adds autobind decorator to handler function

--- packages/typescript-prj00/src/app.ts
+++ packages/typescript-prj00/src/app.ts
@@ -1,3 +1,22 @@
+function AutoBind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Autobind");
+
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+
+  return adjustedDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -8,21 +27,33 @@ class ProjectInput {
   peopleInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    const inportedNode = document.importNode(this.templateElement.content, true);
+    const inportedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
     this.form = inportedNode.firstElementChild as HTMLFormElement;
     this.form.id = "user-input";
 
-    this.titleInputElement = this.form.querySelector("#title") as HTMLInputElement;
-    this.descriptionInputElement = this.form.querySelector("#description") as HTMLInputElement;
-    this.peopleInputElement = this.form.querySelector("#people") as HTMLInputElement;
+    this.titleInputElement = this.form.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.form.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.form.querySelector(
+      "#people"
+    ) as HTMLInputElement;
 
     this.configure();
     this.renderForm();
   }
 
+  @AutoBind
   private subminHandler(event: Event) {
     event.preventDefault();
 
@@ -39,4 +70,4 @@ class ProjectInput {
   }
 }
 
-const projectInput = new ProjectInput();
+new ProjectInput();
