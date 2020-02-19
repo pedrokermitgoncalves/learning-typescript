eacad0df1da444cc0423cba4dc0008ca291bec4c
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-19 11:48:21 +0000

    adds input elements access and submit handler

--- packages/typescript-prj00/src/app.ts
+++ packages/typescript-prj00/src/app.ts
@@ -3,14 +3,35 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   form: HTMLFormElement;
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    this.form = document.importNode(this.templateElement.content, true)
-      .firstElementChild as HTMLFormElement;
+    const inportedNode = document.importNode(this.templateElement.content, true);
+    this.form = inportedNode.firstElementChild as HTMLFormElement;
+    this.form.id = "user-input";
 
+    this.titleInputElement = this.form.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.form.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.form.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
     this.renderForm();
+  }
+
+  private subminHandler(event: Event) {
+    event.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    //this.form.addEventListener("submit", this.subminHandler.bind(this));
+    this.form.addEventListener("submit", this.subminHandler);
   }
 
   private renderForm() {
