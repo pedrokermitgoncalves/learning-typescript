0a30a10dc576432776c14eca702fde3dd612b3c7
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-20 16:42:44 +0000

    split code into a more manageble folder/files structure

---
+++ packages/typescript-prj00/src/components/project-item.component.ts
@@ -0,0 +1,47 @@
+/// <reference path="base.component.ts"/>
+
+namespace App {
+  // Project item clas
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id);
+      this.project = project;
+
+      this.configure();
+
+      this.renderContent();
+    }
+
+    @AutoBind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData("text/plain", this.project.id);
+      event.dataTransfer!.effectAllowed = "move";
+    }
+
+    @AutoBind
+    dragEndHandler(_event: DragEvent) {
+      console.log("drag end");
+    }
+
+    get persons() {
+      return this.project.people === 1
+        ? "1 person"
+        : `${this.project.people} persons`;
+    }
+
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler);
+      this.element.addEventListener("dragend", this.dragEndHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector(
+        "h3"
+      )!.textContent = `${this.persons} assigned`;
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+}
---
+++ packages/typescript-prj00/src/decorators/autobind.ts
@@ -0,0 +1,19 @@
+namespace App {
+  //Autobind
+  export function AutoBind(
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+
+    return adjustedDescriptor;
+  }
+}
