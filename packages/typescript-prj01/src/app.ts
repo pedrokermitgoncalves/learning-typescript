e328d4912929b44c991652f0470e17661f8cc8d3
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-21 12:07:06 +0000

    add validation decorators

--- packages/typescript-prj01/src/app.ts
+++ packages/typescript-prj01/src/app.ts
@@ -1,1 +1,27 @@
-// Code goes here!
+import "reflect-metadata";
+//import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
+import { validate } from "class-validator";
+
+const newProd = new Product("", -5);
+
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformatioon());
+  }
+});
+
+// const products = [
+//   { title: "Carpet", price: 29.99 },
+//   { title: "Book", price: 10.99 }
+// ];
+
+// const p1 = new Product("book", 15.99);
+// console.log(p1.getInformatioon());
+
+//const loadedProducts = plainToClass(Product, products);
+// for (const product of loadedProducts) {
+//   console.log(product.getInformatioon());
+// }
