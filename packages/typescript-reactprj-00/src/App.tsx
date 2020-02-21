27993c93001b8be54de48300b0ab94f528c5f5a6
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-21 17:36:34 +0000

    adds form for new todo

--- packages/typescript-reactprj-00/src/App.tsx
+++ packages/typescript-reactprj-00/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: 1, text: "Finish course" }];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
