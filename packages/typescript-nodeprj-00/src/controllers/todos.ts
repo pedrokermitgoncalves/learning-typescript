95b08f3be2c50415b185659e1006340a31b82cbe
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-26 15:26:08 +0000

    add crud actions to the todo BE

--- packages/typescript-nodeprj-00/src/controllers/todos.ts
+++ packages/typescript-nodeprj-00/src/controllers/todos.ts
@@ -11,3 +11,38 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: "Created todo.", createTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(201).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Couldn't found todo :(");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res
+    .status(201)
+    .json({ message: "Updated todo.", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Couldn't found todo :(");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.status(201).json({ message: "Todo deleted!" });
+};
