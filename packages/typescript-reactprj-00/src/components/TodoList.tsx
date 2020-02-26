0bd9c8561d42f916ceba92d449fbb68f4a06a194
Pedro Kermit Gonçalves <pedro.goncalves@talkdesk.com>
2020-02-26 11:35:46 +0000

    add delete todo logic

--- packages/typescript-reactprj-00/src/components/TodoList.tsx
+++ packages/typescript-reactprj-00/src/components/TodoList.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-
-interface Todo {
-  id: number;
-  text: string;
-}
+import { Todo } from "../todo.model";
 
 interface TodoListProps {
   items: Todo[];
+  onDeleteTodo: (todoId: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = props => {
   return (
     <ul>
       {props.items.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
